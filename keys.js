b5cf77a2a6eb40e6b46163b5a24b7a3c2aeccad1
Shubhanshu Kumar <155217044+ShubhanshuKr@users.noreply.github.com>
2024-03-10 21:56:23 +0530

    Insta- backend

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports = {
+    username: "Shubhanshu_Sawarn",
+    password: "Rishu@1979",
+    dbName: "cluster0",
+    mongoUrl: `mongodb+srv://${encodeURIComponent("Shubhanshu_Sawarn")}:${encodeURIComponent("Rishu@1979")}@cluster0.806zmos.mongodb.net/${"cluster0"}?retryWrites=true&w=majority`,
+    Jwt_secret: "faslkfocvneofu"
+};
